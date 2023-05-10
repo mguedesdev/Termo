6aef424b047871422f43cec1baff16b53eee8cc4
MathheusGuedes <mathheus.gr@gmail.com>
2023-05-10 19:07:50 -0300

    versao-completa

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Termo
+#Jogo Termo
+
+Este código implementa um jogo de adivinhação de palavras com interface simples e teclado virtual. O jogador deve digitar uma palavra de 5 letras e verificar se ela corresponde à palavra escolhida aleatoriamente pelo programa.
+
+## Como o jogo funciona
+
+O jogo consiste em adivinhar uma palavra de 5 letras escolhida aleatoriamente pelo programa. O jogador tem 5 tentativas para adivinhar a palavra correta. O quadro de exibição mostra as tentativas do jogador e a palavra correta.
+
+O jogador pode selecionar a posição no quadro e usar o teclado virtual para inserir letras. Após preencher a palavra, o jogador deve pressionar "ENTER" para verificar se a palavra inserida corresponde à palavra correta. O jogo fornecerá dicas visuais para ajudar o jogador:
+
+- Se a letra inserida estiver na posição correta, o quadrado ficará verde.
+- Se a letra inserida estiver na palavra, mas na posição errada, o quadrado ficará amarelo.
+- Se a letra inserida não estiver na palavra, o quadrado ficará cinza.
+
+O jogador pode apagar a última letra inserida usando o botão "⌫" (apagar) do teclado virtual.
+
+O jogo termina quando o jogador adivinha a palavra correta (ganha) ou esgota todas as 5 tentativas (perde). Um alerta será exibido informando o resultado e o quadro será limpo para iniciar um novo jogo.
+
+
+## Funções
+
+1. `generateBoard()`: Cria o quadro de exibição das palavras. Cria 6 linhas (5 para as tentativas do jogador e 1 para a resposta correta) com 5 colunas cada (1 para cada letra da palavra).
+
+2. `generateKeyboard()`: Cria o teclado virtual com as letras do alfabeto e os botões "ENTER" e "⌫" (apagar).
+
+3. `addSelectedClass()`: Adiciona a classe "selected" ao primeiro elemento do quadro. Além disso, adiciona um evento de clique a todos os elementos do quadro, permitindo ao jogador selecionar a posição a ser preenchida.
+
+4. `addLetterClickEvent()`: Adiciona um evento de clique aos botões de letra do teclado virtual, permitindo que o jogador insira a letra selecionada na posição selecionada no quadro.
+
+5. `btnErrase()`: Adiciona um evento de clique ao botão "⌫" (apagar) do teclado virtual, permitindo que o jogador apague a última letra inserida.
+
+6. `btnSubmit()`: Adiciona um evento de clique ao botão "ENTER" do teclado virtual, permitindo que o jogador envie a palavra atual e verifique se ela corresponde à palavra correta.
+
+7. `startGame()`: Inicializa o jogo, selecionando aleatoriamente uma palavra do objeto `palavras`.
+
+8. `checkWord(typedWord)`: Verifica se a palavra inserida pelo jogador é igual à palavra correta. Se a letra estiver na posição correta, o quadrado ficará verde; se a letra estiver na palavra, mas na posição errada, o quadrado ficará amarelo; se a letra não estiver na palavra, o quadrado ficará cinza.
+
+9. `changerow()`: Muda a linha atual do quadro após cada tentativa do jogador.
+
+10. `endGame(typedWord, selectedWord)`: Verifica se o jogador ganhou (adivinhou a palavra correta) ou perdeu (esgotou todas as tentativas). Exibe um alerta com o resultado e remove a classe "selected" e "rowSelected" do quadro.
+
--- script.js
+++ script.js
@@ -1,112 +1,246 @@
-let wordBoard = document.querySelector('.wordBoard');
-let btnSubmit = document.querySelector('#btnSubmit');
-let chanceH2 = document.querySelector('.chances');
+const wBoard = document.querySelector('.wBoard');
+const kBoard = document.querySelector('.kBoard');
+const test = document.querySelector('.teste');
 
-let correctWord = [
-  'muito',
-  'sonho',
-  'moral',
-  'tempo',
-  'dizer',
-  'causa',
-  'amigo',
-  'sobre',
-  'mundo',
-  'censo',
-  'regra',
-  'ontem',
-];
 
-let chances = 5;
 
-//sortear palavra aleatoria do array de palavras.
-let indexDrawn = Math.floor(Math.random() * correctWord.length );
-let wordDrawn = correctWord[indexDrawn].toUpperCase();
-
-console.log(wordDrawn);
-
-function generateBoard(){
-  //gerar nova linha com 5 quadros somente quando o botão é clicado
-  const line = document.createElement('div');
-    for (let index = 0; index < 5; index += 1) {
-      const column = document.createElement('input');
-      column.maxLength = 1;
-      column.classList.add('cell');
-      line.appendChild(column);
-    }
-  wordBoard.appendChild(line);
+const palavras = { furia: 'fúria', calda: 'calda', manto: 'manto', censo: 'censo', codex: 'codex', facil: 'fácil', fusao: 'fusão', nenem: 'neném', navio: 'navio', vacuo: 'vácuo', balde: 'balde', banco: 'banco', barco: 'barco', bicho: 'bicho',
 }
+let selectedWord = {};
 
-function disableLine(arrayCell){
-  //Desativar linha anterior
-  if(arrayCell.length !== 0){
-    for(let count = 0; count < arrayCell.length; count += 1){
-      arrayCell[count].setAttribute('disabled', 'true');
+const generateBoard = () => {
+  for (let row = 0; row < 6; row++) {
+    const rowDiv = document.createElement('div');
+    rowDiv.id = row+1;
+    if(row === 0) rowDiv.classList.add('rowSelected');
+    for(let index = 0; index < 5; index += 1){
+      const div = document.createElement('div');
+      div.classList.add('letterBoard');
+
+      if(row > 0){
+        div.classList.add('wHidden');
+      }else{
+        div.classList.add('wShow');
+      }
+      rowDiv.appendChild(div);
     }
+    wBoard.appendChild(rowDiv);
   }
+
 }
-
-function paintCell(arrayCell){
-  //aux para fazer a palavra montada voltar ao inicio já que pegamos todas as cells -
-  //e o index vai acabar ultrapassando o tamanho da palavra
-  let aux = 0;
-  for(let index = 0; index < arrayCell.length; index +=1){
-
-    let typedLetterValue = arrayCell[index].value.toUpperCase();
-
-    if(aux > 4) aux = 0;
-    if(typedLetterValue === wordDrawn[aux]){
-      arrayCell[index].style.backgroundColor = 'rgb(3, 68, 34)';
-      arrayCell[index].style.color = 'white';
-    }else if(wordDrawn.includes(typedLetterValue)){
-      arrayCell[index].style.backgroundColor = 'rgb(211,145,0)';
-      arrayCell[index].style.color = 'white';
-    }
-
-    aux += 1;
-  }
-}
-
-function checkWord(string, arrayCell){
-
-  //verificação se ganhou perdeu ou continua o jogo
-  
-  if(string.toUpperCase() === wordDrawn){
-    chanceH2.innerText = 'Você ganhou !';
-    paintCell(arrayCell);
-  }else if (chances < 2){
-    alert('Você Perdeu :(');
-    chanceH2.innerText = 'A palavra é: ' + wordDrawn;
-  }else{
-    chances -= 1;
-    chanceH2.innerText = 'Chances: ' + chances;
-    disableLine(arrayCell);
-    paintCell(arrayCell);
-    generateBoard();
-  }
-}
-
-
-btnSubmit.addEventListener('click', () => {
-
-  const arrayCell = document.querySelectorAll('.cell');
-
-  //montar a palavra
-  let newString = '';
-  for (let count = 0; count < arrayCell.length; count += 1) {
-    if(arrayCell[count].disabled != true){
-      newString += arrayCell[count].value;
-    }
-  }
-  
-  //verificar se a palavra existe no array
-  if(!correctWord.includes(newString)){
-    alert('Não temos essa palavra no nosso banco - tente novamente');
-  }
-  else{
-    checkWord(newString, arrayCell);
-  }
-  
-})
 
 generateBoard();
+const letterArray = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+                      'A', 'S','D', 'F', 'G', 'H', 'J', 'K', 'L', '⌫',
+                        'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'ENTER'];
+
+const generateKeyboard = () => {
+    for(let index = 0; index < 3; index += 1){
+      const keyboardRow = document.createElement('div');
+      keyboardRow.classList.add('keyboardRow');
+      kBoard.appendChild(keyboardRow);
+    }
+
+    const keyboardRows = document.querySelectorAll('.keyboardRow');
+    let rowCounter = 0;
+
+    for(let letter = 0; letter < letterArray.length; letter+=1){
+
+      if(letter === 10 || letter === 20) rowCounter += 1;
+
+      const btn = document.createElement('button');
+      btn.classList.add('letter');
+      btn.innerHTML = letterArray[letter];
+
+      if(letterArray[letter] === 'ENTER') btn.classList.add('btnEnter');
+      if(letterArray[letter] === '⌫') btn.classList.add('btnBackSpace');
+
+      keyboardRows[rowCounter].appendChild(btn);
+    }
+}
+
+generateKeyboard();
+
+//Dia 2 
+
+const addSelectedClass = () => {
+  const wordElems = document.querySelectorAll('.letterBoard');
+  const firstWord = wordElems[0];
+  firstWord.classList.add('selected');
+
+  for (const word of wordElems) {
+    word.addEventListener('click', (event) => {
+      if(word.classList.contains('wShow')){
+        const selectedWord = document.querySelector('.selected');
+        selectedWord.classList.remove('selected');
+        event.target.classList.add('selected');
+      }
+    })
+  }
+}
+
+addSelectedClass();
+
+const addLetterClickEvent = () => {
+  const letterButtons = document.querySelectorAll('.letter');
+
+  for (let index = 0; index < letterButtons.length; index += 1) {
+    if(letterButtons[index].className === 'letter'){
+      letterButtons[index].addEventListener('click', (event) => {
+        const selectedLetter = document.querySelector('.selected');
+        if(selectedLetter){
+          selectedLetter.innerHTML = event.target.innerText;
+
+          if(selectedLetter.nextElementSibling){
+            selectedLetter.classList.remove('selected');
+            selectedLetter.nextElementSibling.classList.add('selected');
+          }
+        }
+      })
+    }
+  }
+}
+
+addLetterClickEvent();
+
+const btnErrase = () => {
+  const btnBackSpace = document.querySelector('.btnBackSpace');
+
+  btnBackSpace.addEventListener('click', () => {
+    test.style.display = 'none';
+    const selected = document.querySelector('.selected');
+    
+      if(selected && !selected.nextSibling && selected.innerText !== ''){
+        selected.innerText = '';
+      }else if(selected && selected.previousSibling){
+        selected.classList.remove('selected');
+        selected.previousSibling.classList.add('selected');
+        selected.previousSibling.innerText = '';
+      }
+  })
+  
+}
+btnErrase();
+
+const btnSubmit = () => {
+  const btnEnter = document.querySelector('.btnEnter');
+  btnEnter.addEventListener('click', () => {
+  test.style.display = 'none';
+  const word = document.querySelectorAll('.wShow');
+  let typedWord = '';
+    for (let index = 0; index < word.length; index += 1) {
+      typedWord += word[index].innerText;
+    }
+    
+    if(typedWord.length < 5){
+      closeAlert('Somente palavras de 5 letras!');
+
+    }else if(!Object.keys(palavras).includes(typedWord.toLowerCase())) {
+      closeAlert('Essa palavra não é aceita');
+
+    }else{
+      checkWord(typedWord);
+      changerow();
+    }
+  });
+}
+btnSubmit();
+
+
+const startGame = () => {
+  const wordKeys = Object.keys(palavras);
+  const randomIndex = Math.floor(Math.random() * wordKeys.length);
+  selectedWord[wordKeys[randomIndex]] = palavras[wordKeys[randomIndex]];
+  console.log(selectedWord);
+}
+startGame();
+
+const checkWord = (typedWord) => {
+  const letters = document.querySelectorAll('.wShow');
+  const selectedWordKey = Object.keys(selectedWord)[0];
+  const selectedWordValue = selectedWord[selectedWordKey].toUpperCase();
+  
+  for (let index = 0; index < letters.length; index += 1) {
+    const selectedLetterKey = selectedWordKey[index].toUpperCase();
+    const correctLetterValue = selectedWordValue[index];
+
+    if(letters[index].innerText === selectedLetterKey){
+      letters[index].innerText = correctLetterValue;
+      letters[index].style.backgroundColor = '#3AA394';
+      letters[index].style.borderColor = '#3AA394';
+    }else if(selectedWordKey.toUpperCase().includes(letters[index].innerText)){
+      letters[index].style.backgroundColor = '#D3AC69';
+      letters[index].style.borderColor = '#D3AC69';
+    }
+    else{
+      letters[index].style.backgroundColor = '#312A2C';
+      letters[index].style.borderColor = '#312A2C';
+    }
+  }
+  endGame(typedWord, selectedWordKey);
+}
+
+//Função para trocar a keyboardRow toda vez que apertarmos o botão ENTER
+const changerow = () => {
+  const word = document.querySelectorAll('.wShow');
+  const selected = document.querySelector('.selected');
+  const rowSelected = document.querySelector('.rowSelected');
+
+  for (const w of word) {
+    w.classList.add('wHidden');
+    w.classList.remove('wShow');
+  }
+  
+    let id = Number(rowSelected?.id) + 1;
+
+    if(id <= 6){
+      rowSelected?.classList.remove('rowSelected');
+
+      const newrow = document.getElementById(id);
+      newrow.classList.add('rowSelected');
+      const newWord = newrow.querySelectorAll('.wHidden');
+
+      for(let index = 0; index < newWord.length; index += 1){
+        newWord[index].classList.add('wShow');
+        newWord[index].classList.remove('wHidden');
+      }
+      selected.classList.remove('selected');
+      newWord[0].classList.add('selected');
+    }
+
+}
+
+const endGame = (typedWord, selectedWord) => {
+  const typedWordUpper = typedWord.toUpperCase();
+  const selectedWordUpper = selectedWord.toUpperCase();
+  const rowSelected = document.querySelector('.rowSelected');
+  const selected = document.querySelector('.selected');
+  const btnEnter = document.querySelector('.btnEnter');
+
+
+  if(typedWordUpper === selectedWordUpper){
+    closeAlert('Você ganhou !');
+    btnEnter.disabled = true;
+    rowSelected.classList.remove('rowSelected');
+    selected.classList.remove('selected');
+  }
+
+  if(rowSelected.id >= 6){
+    closeAlert(`Você perdeu ! A palavra é : ${selectedWordUpper}`);
+    rowSelected.classList.remove('rowSelected');
+    selected.classList.remove('selected');
+  }
+}
+
+
+const closeAlert = (string) => {
+  const span = document.querySelector('.txtAlert');
+  const btnExitAlert = document.querySelector('.exitAlert');
+
+  span.innerText = string;
+  test.style.display = 'block';
+  btnExitAlert.addEventListener('click', () => {
+    test.style.display = 'none';
+  })
+
+}
